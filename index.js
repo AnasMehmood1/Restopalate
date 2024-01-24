dc8b7835c7d6f73eee4db4e5cd3f9b5c5dfa3477
AnasMehmood1 <anasmehmood167@gmail.com>
2024-01-24 19:41:32 +0500

    Working

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ hamburger.addEventListener("click", () => {
     menu.classList.toggle("active");
     hamburger.classList.toggle("active")
 
-    // Check if the menu is active to toggle the icon
+    
     if (menu.classList.contains("active")) {
         hamburger.innerHTML = '<box-icon name="x" color="#ffffff" class="size"></box-icon>';
     } else {
@@ -21,7 +21,7 @@ function header() {
     const navlink = document.querySelector(".nav-links");
     var headerHeight = header.offsetHeight;
 
-    window.addEventListener('scroll', ()=> {
+    window.addEventListener('scroll', () => {
         if (window.scrollY >= headerHeight) {
             logo.classList.add("active");
             navlink.classList.add("active");
@@ -35,3 +35,26 @@ function header() {
 }
 
 header();
+
+function Restaurent() {
+    var navbar = document.querySelector(".navbar");
+    const logo = document.querySelector(".logo");
+    const navlink = document.querySelector(".nav-links");
+
+    var headerHeight = document.querySelector(".restaurent-header").offsetHeight;
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY >= headerHeight) {
+            logo.classList.add("active");
+            navlink.classList.add("active");
+            navbar.classList.add("sticky");
+        } else {
+            navbar.classList.remove('sticky');
+            logo.classList.remove("active");
+            navlink.classList.remove("active");
+        }
+    });
+}
+
+
+Restaurent();
